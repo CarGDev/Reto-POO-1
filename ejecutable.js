2beef57bad8a296bf65b17aeb8853d82e39dfa54
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-10 17:27:57 -0500

    Modifying the class ejecutable

--- ejecutable.js
+++ ejecutable.js
@@ -10,28 +10,29 @@ class ejecutable extends cuenta {
   }
 
   set ingresar (_cantidad) {
-    this.cantidadRetirar = _cantidad
+    if (_cantidad < 1) {
+      this.cantidadRetirar = 0
+    } else if (_cantidad > this.cantidad) {
+      this.cantidadRetirar = this.cantidad
+    } else {
+      this.cantidadRetirar = _cantidad
+    }
   }
 
   get toString () {
     const print = this.resta()
-    return console.log(`${this.titular} tienes en tu cuenta ${print}`)
+    return console.log(`${this.titular} tenias ${this.cantidad} ahora tienes en tu cuenta ${print}, retiraste ${this.cantidadRetirar}`)
   }
 
   resta() {
-    if (this.cantidadRetirar > 0) {
-      const total = this.cantidad - this.cantidadRetirar
-      if (total < 1) return 0
-      return total
-    } else {
-      return this.cantidad
-    }
+    const total = this.cantidad - this.retirar
+    return total
   }
 }
 
 
-const ingreso = new ejecutable('Carlos Gutierrez')
-ingreso.ingresar = 50
+const ingreso = new ejecutable('Carlos Gutierrez', 58)
+ingreso.ingresar = 60
 ingreso.toString
 
 const ingreso2 = new ejecutable('Arturo Gonzalez', 100)
